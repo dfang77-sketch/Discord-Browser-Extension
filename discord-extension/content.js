ddc81bb223419ce80c8c4d30b1db153cfa355ae1
Fang <dfang77@icloud.com>
2026-08-05 17:38:50 +1000

    Update content.js

--- discord-extension/content.js
+++ discord-extension/content.js
@@ -1,16 +1,20 @@
-
-
 (function () {
   'use strict';
 
-  const script = document.createElement('script');
-  script.src = chrome.runtime.getURL('injected.js');
-  script.type = 'text/javascript';
-  (document.head || document.documentElement).appendChild(script);
-  script.onload = () => script.remove();
+ 
+  const url = chrome.runtime.getURL('injected.js');
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url, false); 
+  xhr.send();
 
+  const script = document.createElement('script');
+  script.textContent = xhr.responseText;
+  (document.head || document.documentElement).appendChild(script);
+  script.remove();
+
+ 
   window.addEventListener('message', (event) => {
     if (event.source !== window) return;
-    if (!event.data?.source === 'discord-enhancer') return;
+    if (event.data?.source !== 'discord-enhancer') return; 
   });
 })();
